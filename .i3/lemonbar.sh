e0632dbfe230dd1c6d180d021f4a7d9c1fa1da12
Miguel <miguel@localhost.localdomain>
2015-09-24 01:39:22 +0100

    changed colors, added mpd play time

--- .i3/lemonbar.sh
+++ .i3/lemonbar.sh
@@ -2,7 +2,7 @@ PANEL_FIFO="/tmp/i3_lemonbar_${USER}"
 FONT1="source code pro for powerline:regular:size=9"
 FONT2="Font Awesome::regular:size=10"
 DEVICE=$(iw dev | grep Interface | awk '{print $2}')
-SONG_MAX_LENGTH=35
+SONG_MAX_LENGTH=30
 H_GAP=20
 V_GAP=10
 BAR_HEIGHT=18
@@ -25,19 +25,19 @@ mkfifo "${PANEL_FIFO}"
 
 $(dirname $0)/i3_workspaces.py > "${PANEL_FIFO}" &
 
-while true; do
-    echo "VOL$(amixer get Master | awk -F"[]%[]" '/%/ { sum += $2; n++ } END { if (n > 0) print sum/n;}')";
-    sleep 1;
+while :; do
+    echo "VOL$(amixer get Master | awk -F"[]%[]" '/%/ { sum += $2; n++ } END { if (n > 0) print sum/n;}')"
+    sleep 1
 done > $PANEL_FIFO &
 
-while true; do
-    echo "DATE$(date +"%A, %d %B %Y")";
-    echo "TIME$(date +"%R")";
-    sleep 5;
+while :; do
+    echo "DATE$(date +"%A, %d %B %Y")"
+    echo "TIME$(date +"%R")"
+    sleep 5
 done > $PANEL_FIFO &
 
-while true; do
-    contents=$(mpc --format "%artist% - %title%" | head -1)
+while :; do
+    contents=$(mpc current --format "%artist% - %title%")
     if [ "$prev_contents" != "$contents" ]; then
         prev_contents=${contents}
         count=0
@@ -46,8 +46,8 @@ while true; do
         stop=1
     fi
     if [ ${#contents} -gt ${SONG_MAX_LENGTH} ]; then
-        end=$((count+SONG_MAX_LENGTH-1))
-        song="$(echo ${contents} | cut -c${count}-${end})⛾"
+        end=$((count+SONG_MAX_LENGTH))
+        song=${contents:${count}:${SONG_MAX_LENGTH}}
         if [ ${stop} -eq 0 ]; then
             timer=$((timer+1))
             if [ ${timer} -eq 5 ]; then
@@ -59,7 +59,7 @@ while true; do
             if [ ${end} -eq ${#contents} ]; then
                 stop=0
                 increment=-1
-            elif [ ${count} -eq 1 ]; then
+            elif [ ${count} -eq 0 ]; then
                 stop=0
                 increment=1
             fi
@@ -68,14 +68,13 @@ while true; do
     else
         song=${contents}
     fi
-    echo "MUS$(mpc | awk 'NR==2 {print $1}')${song}"
-    sleep 0.25;
+    echo "MUS$(mpc | awk 'NR==2 {print $1}')${song} [$(mpc | awk 'NR==2 {print $3}')]"
+    sleep 0.25
 done > $PANEL_FIFO &
 
-while true; do
-    echo "SSID$(iw dev ${DEVICE} link | grep SSID | awk '{print $2}')";
-    echo "SIG$(iw dev ${DEVICE} link | grep signal | awk '{print $2}')";
-    sleep 5;
+while :; do
+    echo "SSID$(iw dev ${DEVICE} link | grep SSID | awk '{print $2}')"
+    sleep 5
 done > $PANEL_FIFO &
 
 cat "${PANEL_FIFO}" | $(dirname $0)/lemonbar_parser.sh | lemonbar -g ${BAR_LENGTH}x${BAR_HEIGHT}+${H_GAP}+${V_GAP} -d -f "${FONT1}" -f "${FONT2}" | sh
